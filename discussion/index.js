c08fe457b847681c6a1033f1ee2ab1861084e1b7
avrilbelisario <belisarioaj@students.nu-dasma.edu.ph>
2023-09-12 17:52:59 +0800

    Initial commit

---
+++ discussion/index.js
@@ -0,0 +1,29 @@
+
+	let fullName = "Avril Jhoanna Ashanti J. Belisario";
+	console.log("My full name is" + name);
+
+	let age = 20;
+	console.log("My current age is: " + currentAge);
+	
+	let friends = ["Jhanna","Luis","Oteph","Gely","Trisha"];
+	console.log("My Friends are: ")
+	console.log(friends);
+
+	let profile = {
+
+		username "avrilbelisario"
+		fullName: "Avril Belisario';
+		age: 20,
+		isActive: false,
+
+	}
+	console.log("My Full Profile: ")
+	console.log(profile);
+
+	let fullName = "Zelle Santos";
+	console.log("My bestfriend is: " + fullName);
+
+	const lastLocation = "SM Dasma";
+	lastLocation = "SM Dasma";
+	console.log("I was found frozen in: " + lastLocation);
+}
